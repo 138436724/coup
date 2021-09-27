e3262d0b882b2dde96ad88bacb20dd1fca8a1fa1
FireFight <2268561366@qq.com>
2021-09-27 23:16:19 +0800

    test

--- src/plugins/coup/__init__.py
+++ src/plugins/coup/__init__.py
@@ -1,102 +1,189 @@
-from random import shuffle
-from parse import parse
+"""
+这里接收所有与政变有关的命令, 具体事务由master处理
+"""
 
 from nonebot.rule import to_me
 from nonebot import on_command, on_endswith, on_startswith, on_regex
 from nonebot.typing import T_State
-from nonebot.adapters.cqhttp import Bot, Event, MessageSegment
-from .game_features import *
-# from .identity_cards import *
-from .player import Player
+from nonebot.adapters.cqhttp import Bot, Event
+from .game_features import check_num
 
-# 游戏是否开始
-game_flag = False
-# 玩家数据
-players = {}
-# 身份牌
-identities = ['公爵 ', '队长 ', '夫人 ', '刺客 ', '大使 '] * 4
-# 身份牌墓地
-card_graveyard = []
+from master import Master
 
-help_documentation = on_command("帮助", rule=to_me(), priority=0)
+masters = {}  # 存储所有房间, 存储方式为 {房间号: 对应房间}
+all_player = {}  # 存储所有的玩家, 存储方式为 {玩家QQ号: 对应房间号}
+# [受害者QQ, 操作, 操作人QQ, 阻止人QQ, 质疑人QQ(""代表无人)]
+action_chain = ["", "", "", "", ""]
+is_block = False  # 是否阻止
+identity_card = ""  # 需要换掉的身份
 
-start_game = on_command("开始", rule=to_me(), priority=1)
-draw_cards = on_endswith("人", rule=to_me(), priority=1)
-give_cards = on_regex("^[1-7一二三四五六七两俩]", rule=to_me(), priority=2)
-change_cards = on_startswith("换牌", rule=to_me(), priority=2)
-die_cards = on_startswith("死亡", rule=to_me(), priority=2)
-end_game = on_command("结束", rule=to_me(), priority=1)
+# ======================基础元素====================== #
+# 帮助文档
+Docs = on_command("帮助", rule=to_me(), aliases={"帮助文档", }, priority=0)
 
 
-@help_documentation.handle()
-async def _(bot: Bot, event: Event):
-    text = '请先呼我名\n' \
-           '创建房间命令是”开始“\n' \
+@Docs.handle()
+async def _(bot: Bot):
+    text = '创建房间命令是”开始“\n' \
            '选择人数命令是“n人”\n' \
            '加入房间的命令是“n“\n' \
            '换牌命令是”换牌第n张“\n' \
+           '可以用“查询”来查询各位的币数\n' \
            '死亡命令是”死亡第n张“\n' \
            '支持命令”结束“快速结束\n' \
            '详情规则参考http://www.yihubg.com/rule-details/7f81295f-7160-4261-b436-d385395b9b22'
-    await help_documentation.finish(text)
+    await Docs.finish(text)
 
 
-@start_game.handle()
+# 玩家人数
+players_num = on_endswith("人", rule=to_me(), priority=1)
+
+
+@players_num.handle()
 async def _(bot: Bot, event: Event, state: T_State):
-    global game_flag, players, identities
-    game_flag = True
-    players.clear()
-    shuffle(identities)
-    await start_game.finish("开始你们的表演")
+    global masters, all_player
+    qq_number = event.get_user_id()
+    if all_player.get(qq_number, ""):
 
-
-# 准备身份牌
-@draw_cards.handle()
-async def _(bot: Bot, event: Event, state: T_State):
-    global game_flag, players, identities
-    if game_flag:
-        if len(players) != 0:
-            await draw_cards.finish("嗯?")
         num = await check_num(str(event.get_message()))
-        if num != -1:
-            for i in range(num):
-                participant = Player()
-                participant.identity = identities[0:2]
-                del identities[0:2]
-                players[i] = participant
-        else:
-            await draw_cards.finish("序号错误")
-    else:
-        await draw_cards.finish("先开始游戏哦")
+        if 2 <= num <= 7:
+            # 为房主分发身份牌
+            master_ = Master(num)
+            cards = await master_.draw_cards(qq_number)
 
-
-# 分发身份牌
-@give_cards.handle()
-async def _(bot: Bot, event: Event, state: T_State):
-    global game_flag, players, identities
-    if game_flag:
-        num = await check_num(str(event.get_message()))
-        if num != -1:
-            qq_number = event.get_user_id()
-            # print(qq_number)
-            if num - 1 in players.keys() and qq_number not in players.keys():
-                players[qq_number] = players.pop(num - 1)
-                await give_cards.finish(user_id=int(qq_number), message=" ".join(players[qq_number].identity),
-                                        message_type="private")
+            if cards:
+                # 私聊发送身份牌
+                await bot.send_private_msg(user_id=int(qq_number), message="".join(cards))
+                # 将房主纳入参与者名单
+                all_player[qq_number] = qq_number
+                # 用房主QQ表作为房间号
+                masters[qq_number] = master_
+                await players_num.finish(f"创建的房间号为{qq_number}")
             else:
-                await give_cards.finish("有人选了哦")
+                await players_num.finish("人数已满!")
+
         else:
-            await give_cards.finish("序号错误")
+            await players_num.finish("人数错误!")
+
     else:
-        await give_cards.finish("请先开始游戏")
-        # await give_cards.finish("")
-        # print(players)
+        await players_num.finish(f"你已经在{all_player[qq_number]}房间里面了哦")
+
+
+# 加入房间自动分配顺序
+join_in_room = on_startswith("进", rule=to_me(), priority=1)
+
+
+# 玩家是否已经在某个房间里面了，房间是否存在，房间是否满员
+@join_in_room.handle()
+async def _(bot: Bot, event: Event):
+    global masters, all_player
+    qq_number = event.get_user_id()
+    # 是否已经加入房间
+    if all_player.get(qq_number, ""):
+        # 从命令中取得房号
+        room_number = str(event.get_message())[1:]
+        room = await masters.get(room_number, None)
+
+        if room:
+            # 分发身份牌
+            cards = room.draw_cards(qq_number)
+
+            if cards:
+                # 纳入参与者名单
+                all_player[qq_number] = room_number
+                # 私聊发送身份牌
+                await players_num.finish(user_id=int(qq_number), message=" ".join(cards), message_type="private")
+
+            else:
+                await join_in_room.finish("房间已满，换一个吧")
+
+        else:
+            await join_in_room.finish("房间号错误!")
+
+    else:
+        await players_num.finish(f"你已经在{all_player[qq_number]}房间里面了哦")
+
+
+# 查询现在在场玩家的打开身份牌和钱币
+inquire_info = on_command("查询", priority=1)
+
+
+@inquire_info.handle()
+async def _(bot: Bot, event: Event):
+    global masters, all_player
+    # 获取这个玩家所在的房间号
+    qq_number = event.get_user_id()
+    room_number = all_player.get(qq_number, "")
+
+    if room_number and await masters[room_number].is_full_people():
+        # 获取这个房间里面所有玩家的信息
+        msg = await masters[room_number].players_info()
+        await inquire_info.finish(msg)
+    else:
+        await inquire_info.finish("你是来找茬的是不是?")
+
+
+end_game = on_command("结束", rule=to_me(), priority=1)
+
+
+# 用于玩家自己结束游戏
+@end_game.handle()
+async def _(bot: Bot, event: Event, state: T_State):
+    global masters, all_player
+    # 获取房间号
+    qq_number = event.get_user_id()
+    room_number = all_player.get(qq_number, "")
+
+    if room_number:
+        # 销毁该对象
+        del masters[room_number]
+        await end_game.finish("再来一把?")
+    else:
+        await inquire_info.finish("你是来找茬的是不是?")
+
+
+# ======================游戏逻辑====================== #
+# 质疑的处理
+doubt = on_command("质疑", priority=1)
+
+
+@doubt.handle()
+async def _(bot: Bot, event: Event):
+    global masters, all_player, action_chain, is_block, identity_card
+    # 获取房间号
+    qq_number = event.get_user_id()
+    room_number = all_player.get(qq_number, "")
+
+    # 添加质疑人进入操作链
+    action_chain[-1] = qq_number
+
+    # 处理质疑
+    result = await masters[room_number].doubt_event(is_block, action_chain)
+    is_block = False
+    # action_chain = ["", "", "", "", ""]
+    if len(result) == 3:
+        await doubt.finish(f"{result[0]}质疑失败，选择开牌; {result[1]}选择换牌")
+        identity_card = result[-1]
+    else:
+        await doubt.finish(f"质疑成功, {result[0]}选择开牌")
+        identity_card = ""
+
+# 所有操作先加入操作链, 时刻注意is_block和identity_card, 涉及刺杀和政变先检查金币数量
+
+change_cards = on_startswith("换牌", rule=to_me(), priority=2)
+die_cards = on_startswith("死亡", rule=to_me(), priority=2)
+get_one_coin = on_command("收入", aliases={"拿1", "拿一"}, rule=to_me(), priority=2)
+get_two_coins = on_command("援助", aliases={"拿2", "拿二"}, rule=to_me(), priority=2)
+get_three_coins = on_command("公爵", aliases={"拿3", "拿三", "税收"}, rule=to_me(), priority=2)
+lost_two_coins = on_startswith("抢", rule=to_me(), priority=2)
+lost_three_coins = on_startswith("刺杀", rule=to_me(), priority=2)
+lost_seven_coins = on_startswith("政变", rule=to_me(), priority=2)
 
 
 # 换牌
 @change_cards.handle()
 async def _(bot: Bot, event: Event, state: T_State):
-    global game_flag, players, identities
+    global game_start, players, identities
     if game_flag:
         num = await check_num(str(event.get_message()))
         qq_number = event.get_user_id()
@@ -118,7 +205,7 @@ async def _(bot: Bot, event: Event, state: T_State):
 # 死亡身份牌
 @die_cards.handle()
 async def _(bot: Bot, event: Event, state: T_State):
-    global game_flag, players, identities
+    global game_start, players, identities
     if game_flag:
         num = await check_num(str(event.get_message()))
         qq_number = event.get_user_id()
@@ -138,3 +225,82 @@ async def _(bot: Bot, event: Event, state: T_State):
             await die_cards.finish("序号错误")
     else:
         await die_cards.finish("请先开始游戏哦")
+
+
+@get_one_coin.handle()
+async def _(bot: Bot, event: Event, state: T_State):
+    global game_start, players, identities
+    if game_flag:
+        qq_number = event.get_user_id()
+        await players[qq_number].get_one_coin()
+        await get_one_coin.finish(f"你现在有{players[qq_number].wealth}个币了", at_sender=True)
+    else:
+        await get_one_coin.finish("先开始游戏哦")
+
+
+@get_two_coins.handle()
+async def _(bot: Bot, event: Event, state: T_State):
+    global game_start, players, identities
+    if game_flag:
+        qq_number = event.get_user_id()
+        await players[qq_number].get_two_coins()
+        await get_two_coins.finish(f"你现在有{players[qq_number].wealth}个币了", at_sender=True)
+    else:
+        await get_two_coins.finish("先开始游戏哦")
+
+
+# 公爵
+@get_three_coins.handle()
+async def _(bot: Bot, event: Event, state: T_State):
+    global game_start, players, identities
+    if game_flag:
+        qq_number = event.get_user_id()
+        players[qq_number].get_three_coins()
+        await get_three_coins.finish(f"你现在有{players[qq_number].wealth}个币了", at_sender=True)
+    else:
+        await get_three_coins.finish("先开始游戏哦")
+
+
+# 队长
+@lost_two_coins.handle()
+async def _(bot: Bot, event: Event, state: T_State):
+    global game_start, players, identities
+    if game_flag:
+        was_robben_qq_number = parse("抢[CQ:at,qq={}]", str(event.get_message()))[0]  # todo
+        print(int(was_robben_qq_number))
+        qq_number = event.get_user_id()
+        if players[was_robben_qq_number].wealth >= 2:
+            players[qq_number].rob_two_coins(True)
+        players[was_robben_qq_number].was_robben()
+        await lost_two_coins.finish("")
+    else:
+        await lost_two_coins.finish("先开始游戏哦")
+
+
+# 刺客
+@lost_three_coins.handle()
+async def _(bot: Bot, event: Event, state: T_State):
+    global game_start, players, identities
+    if game_flag:
+        qq_number = event.get_user_id()
+        if players[qq_number].wealth >= 3:
+            players[qq_number].stab()
+        else:
+            await lost_three_coins.finish("没钱了诶")
+    else:
+        await lost_three_coins.finish("先开始游戏哦")
+
+
+# 政变
+@lost_seven_coins.handle()
+async def _(bot: Bot, event: Event, state: T_State):
+    global game_start, players, identities
+    if game_flag:
+        coup_qq_number = parse('[CQ:at,qq={}]', str(event.get_message()))  # todo
+        qq_number = event.get_user_id()
+        if players[qq_number].wealth >= 7:
+            players[qq_number].coup()
+        else:
+            await lost_seven_coins.finish("没钱了诶")
+    else:
+        await lost_seven_coins.finish("先开始游戏哦")
--- src/plugins/coup/player.py
+++ src/plugins/coup/player.py
@@ -1,19 +1,58 @@
+"""
+玩家角色信息:
+    币数目、身份牌、身份牌数目
+    玩家的技能: 本身技能和身份技能
+    本身技能: 收入、援助、质疑、政变
+    角色技能: 公爵(税收、阻止)、队长(抢夺、阻止)、大使(换牌，阻止)、刺客(刺杀)、夫人(阻止)
+"""
+
+
 class Player:
-    '''
-    玩家角色信息:
-        币数目
-        身份牌
-        身份牌数目
-        玩家的技能:本身技能和身份技能
-            本身技能:
-                收入
-                援助
-                质疑
-                政变
-            角色技能:
-                公爵
-                队长
-                刺客
-    '''
-    wealth = 0
-    identity = []
+    coins = 2
+    close_identities = []
+    open_identities = []
+
+    # 开牌
+    async def open_card(self, num):
+        identity = self.close_identities.pop(num - 1)
+        self.open_identities.append(identity)
+
+    # 收入
+    async def get_one_coin(self):
+        self.coins += 1
+
+    # 援助
+    async def get_two_coins(self):
+        self.coins += 2
+
+    # 政变
+    async def coup(self):
+        self.coins -= 7
+
+
+    # 公爵(税收、阻止)
+    async def get_three_coins(self):
+        self.coins += 3
+
+    # 队长(抢夺、阻止)
+    async def rob_two_coins(self, num):
+        self.coins += num
+
+    async def was_rob_coins(self):
+        if self.coins >= 2:
+            self.coins -= 2
+            return 2
+        elif self.coins == 1:
+            self.coins = 0
+            return 1
+        else:
+            self.coins = 0
+            return 0
+
+    # 大使(换牌，阻止)
+    async def change_two_cards(self):
+        pass  # todo
+
+    # 刺客(刺杀)
+    async def stab(self):
+        self.coins -= 3
